0bb4d0ef53e7e0814b3f5377e597677638c91dd6
poca1212 <pochip12@naver.com>
2025-03-02 14:13:03 +0900

    Add files via upload

---
+++ BAEKJOON_16430.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	int A, B, P;
+	
+	cin >> A >> B;
+	
+	P = B - A;
+	cout << P << " " << B;
+	return 0;
+
+}
+
