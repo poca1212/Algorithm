69cea8c995488d2a21ff6783e6abf7bec26ef6ea
poca1212 <pochip12@naver.com>
2025-03-16 14:26:18 +0900

    Add files via upload

---
+++ BAEKJOON_12756.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int f, F, h, H;
+
+	cin >> f >> h;
+	cin >> F >> H;
+
+	while (h > 0 && H > 0) {
+		h -= F;
+		H -= f;
+	}
+
+	
+	if (h > 0) {
+		cout << "PLAYER A";
+	}
+	else if (H > 0) {
+		cout << "PLAYER B";
+	}
+	else {
+		cout << "DRAW";
+	}
+}
