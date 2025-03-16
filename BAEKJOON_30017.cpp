0b787a89a56d070f91c2e00add8a54d7f8b3d03d
poca1212 <pochip12@naver.com>
2025-03-16 14:15:51 +0900

    Add files via upload

---
+++ BAEKJOON_30017.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int A, B;
+	int c;
+	cin >> A >> B;
+	c = A - B;
+
+	if (A > B) {
+		cout << B + B + 1;
+	}
+	else if (A < B) {
+		cout << A + A -1;
+	}
+	
+	else {
+		cout << A + B - 1;
+	}
+}
