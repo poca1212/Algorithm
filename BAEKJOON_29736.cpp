e8315139bae0a2576619e99880a48d26332e0dfb
poca1212 <pochip12@naver.com>
2025-03-16 14:55:34 +0900

    Add files via upload

---
+++ BAEKJOON_29736.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int A, B, K, X;
+	int s, b, a, h=0;
+	cin >> A >> B;
+	cin >> K >> X;
+	s = K - X;
+	b = K + X;
+	a = b - s + 1;
+
+	for (int i = s; i <=b; i++) {
+		if (i >= A && i <= B) {
+			h += 1;
+		}
+	}
+
+
+	if (h==0) {
+		cout << "IMPOSSIBLE";
+	}
+	else {
+		cout << h;
+	}
+}
